0c40cc41e28514e9d35125378c6f108ebf271e02
Seonggi Yang <seonggi.yang@gmail.com>
2016-01-17 20:00:54 +0800

    Update

--- elpa/ahg-20150817.5/ahg-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ahg" "20150817.5" "Alberto's Emacs interface for Mercurial (Hg)" 'nil :url "https://bitbucket.org/agriggio/ahg")
---
+++ elpa/ahg-20151223.301/ahg-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ahg" "20151223.301" "Alberto's Emacs interface for Mercurial (Hg)" 'nil :url "https://bitbucket.org/agriggio/ahg")
